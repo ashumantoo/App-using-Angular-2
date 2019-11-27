807ac5f79887304655d6551b92480f0f8e3c7c64
ashutosh <ashutosh@apiria.com>
2019-11-27 13:34:31 +0530

    cross component communication using service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
 import { Recipe } from './recipe.model'
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
         new Recipe('Sweet and Chizze Pizza', 'Full of testy , creaspy and salty pizza', 'https://media.fshoq.com/images/316/pizza-with-vegetables-on-a-wooden-table-316-small.jpg'),
