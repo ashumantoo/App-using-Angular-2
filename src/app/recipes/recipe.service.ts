daa71f02955d41baec1cea8a9411d8d9583238e4
ashutosh <ashutosh@apiria.com>
2019-11-27 15:08:43 +0530

    added ingredients using services.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,39 @@
 import { Recipe } from './recipe.model'
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from 'app/shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-        new Recipe('Sweet and Chizze Pizza', 'Full of testy , creaspy and salty pizza', 'https://media.fshoq.com/images/316/pizza-with-vegetables-on-a-wooden-table-316-small.jpg'),
+        new Recipe(
+            'Recipe',
+            'This is simply a test',
+            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('Frech Fries', 20)
+            ]),
+        new Recipe(
+            'Sweet and Chizze Pizza',
+            'Full of testy , creaspy and salty pizza',
+            'https://media.fshoq.com/images/316/pizza-with-vegetables-on-a-wooden-table-316-small.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1),
+            ]),
     ];
+
+    constructor(private shoppingListService: ShoppingListService) { }
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
