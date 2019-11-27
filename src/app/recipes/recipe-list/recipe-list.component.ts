545d6c601e787ff9d9742557e27e5e5917923ffb
ashutosh <ashutosh@apiria.com>
2019-11-27 13:20:53 +0530

    recipe service added.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,25 +1,21 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe('Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Sweet and Chizze Pizza', 'Full of testy , creaspy and salty pizza', 'https://media.fshoq.com/images/316/pizza-with-vegetables-on-a-wooden-table-316-small.jpg'),
-
-  ];
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
-  
+
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
   }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,12 @@
+import { Recipe } from './recipe.model'
+
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe('Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+        new Recipe('Sweet and Chizze Pizza', 'Full of testy , creaspy and salty pizza', 'https://media.fshoq.com/images/316/pizza-with-vegetables-on-a-wooden-table-316-small.jpg'),
+    ];
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+}
