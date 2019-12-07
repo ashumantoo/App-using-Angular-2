739d581702d54af46c50c3157468e2c496c05f2d
ashutosh <ashumantoo@gmail.com>
2019-12-07 15:00:30 +0530

    minor change.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs";
+import { User } from "./user.model";
+import { tap } from "rxjs/operators";
 
 interface AuthResponseData {
     kind: string,
@@ -12,7 +15,10 @@ interface AuthResponseData {
 }
 @Injectable()
 export class AuthService {
+
+    user = new Subject<User>();
     // FIREBASE_API_KEY = 'AIzaSyANHaVBWQBXzvOpPlm3SG6aPlFeWof6kvk';
+
     constructor(private http: HttpClient) { }
 
     signup(email: string, password: string) {
@@ -22,7 +28,14 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        );
+        )
+        // .pipe(tap(resData => {
+        //     const expirationDate = new Date(
+        //         new Date().getTime() + +resData.expiresIn * 1000
+        //     );
+
+        // })
+        // );
     }
 
     login(email: string, password: string) {
